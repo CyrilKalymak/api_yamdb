9162e8e6e3fbae950770c7058594db3ed1a5b436
Alexandr <kreigun@mail.ru>
2023-03-14 14:45:19 +0300

    Update models.py

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    USER_ROLES = [
+        (USER, 'Пользователь'),
+        (MODERATOR, 'Модератор'),
+        (ADMIN, 'Администратор'),
+    ]
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name='имя пользователя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name='фамилия пользователя'
+    )
+    bio = models.TextField(
+        verbose_name='Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        verbose_name='Роль пользователя',
+        max_length=25,
+        choices=USER_ROLES,
+        default='user',
+    )
+    confirmation_code = models.CharField(
+        max_length=250,
+        blank=True,
+    )
