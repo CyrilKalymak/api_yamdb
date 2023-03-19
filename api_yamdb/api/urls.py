8783f7d22c45c0003ef3de1223bd8cd87b1a5c92
Þëèÿ Øåáàëèíà <yulyasha1984@yandex.ru>
2023-03-19 21:48:20 +0300

    поправила urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -24,5 +24,5 @@ v1_router.register(
 urlpatterns = [
     path("v1/", include(v1_router.urls)),
     path("v1/", include("djoser.urls.jwt")),
-    path("v1/", include("users.urls")),
+#    path("v1/", include('users.urls'),
 ]
