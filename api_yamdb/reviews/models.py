4cde096534d2fa1210be18fb5177db69c806a840
Þëèÿ Øåáàëèíà <yulyasha1984@yandex.ru>
2023-03-21 17:23:52 +0300

    readme

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from api_yamdb.users.models import User
+from users.models import User
 
 
 class Category(models.Model):
