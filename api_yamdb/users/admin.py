17e03cec69a6417b56be71cea24438fcefcd584b
Кирилл <117464140+CyrilKalymak@users.noreply.github.com>
2023-03-27 18:02:54 +0300

    Update admin.py

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -13,9 +13,3 @@ class UserAdmin(admin.ModelAdmin):
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Title)
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(GenreTitle)
-admin.site.register(Review)
-admin.site.register(Comment)
